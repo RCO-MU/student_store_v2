ccfa11a65657f8d41ab029023f17895f4c39aaab
Rafael Collado <rco@fb.com>
2022-06-24 18:21:08 -0700

    re-styling

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -4,8 +4,8 @@ import './Hero.css';
 export default function Hero() {
   return (
     <div className="hero">
-      <h1 className="intro">Welcome!</h1>
       <img src="https://i.imgur.com/yQPOOE3.jpg" className="hero-img" alt="hero" />
+      <h1 className="intro">Welcome!</h1>
     </div>
   );
 }
